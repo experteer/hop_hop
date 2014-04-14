85d7971f745cdfeb2f8e06ade81ed40760a8abbb
Andreas Eger <dev@eger-andreas.de>
2014-04-14 16:38:14 +0200

    exclude rabbitmq specs by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ HopHop::Consumer.receiver=HopHop::TestReceiver.new
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
+  config.filter_run_excluding :rabbitmq
   config.filter_run :focus
 
   # Run specs in random order to surface order dependencies. If you find an
