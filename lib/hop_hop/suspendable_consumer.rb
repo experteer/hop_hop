aeb6e2b528e05cd9f6feea46934f6a171a41c4d5
Morus Walter <morus.walter@experteer.com>
2014-07-08 15:44:21 +0200

    added docs; preserve return value of consumer; make wait queue anonymous

--- lib/hop_hop/suspendable_consumer.rb
+++ lib/hop_hop/suspendable_consumer.rb
@@ -18,14 +18,19 @@ module HopHop
     class << self
       attr_accessor :suspend_message, :suspend
 
+      # declare the message that suspends the consumer
       def suspend_on( message )
         bind message
         self.suspend_message = message
       end
 
+      # declare the message that resumes the consumer
+      #
+      # the method creates an additional, unnamed consumer
+      # that does nothing but wait for the first message
+      # on a temporary queue and terminate
       def resume_on( message )
         @wait_consumer = Class.new(HopHop::Consumer) do
-          queue self.queue
           
           bind message
           
@@ -34,21 +39,26 @@ module HopHop
           end
         end
       end
-    
+
+      # message processing handling the suspend option 
+      # @param [Hash] options options for the consumer
+      # @return [Boolean] true if exit_loop was called, false if loop was exited because of an error
       def consume(options = {})
         while true
           self.suspend = false
-          super(options)
+          ret = super(options)
           if self.suspend
             @wait_consumer.consume
           else
-            break
+            return ret
           end
         end
       end
 
     end
 
+    private
+    # process suspend messages before normal message processing
     def check_suspend(event, info)
 
       if event.name == self.class.suspend_message
